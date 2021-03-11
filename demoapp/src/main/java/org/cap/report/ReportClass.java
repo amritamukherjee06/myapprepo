019e407a836d9267f6f1b568b15fb84fb12a76b6
amritamukherjee06 <amrimukh@LIN83003311.corp.capgemini.com>
2021-03-11 08:13:58 +0530

    Commit Report

--- demoapp/src/main/java/org/cap/report/ReportClass.java
+++ demoapp/src/main/java/org/cap/report/ReportClass.java
@@ -5,5 +5,9 @@ public class ReportClass {
 	public void show_Weekly_Report() {
 		System.out.println("Weekly Report Completed");
 	}
+	
+	public void show_Yearly_Report() {
+		System.out.println("Yearly Report Completed");
+	}
 
 }
